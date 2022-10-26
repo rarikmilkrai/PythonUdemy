8628920aa1e78eb8f6ae54407ef108277d14aa7c
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2022-10-26 19:31:34 -0300

    dia cheio, porem rendeu algo

---
+++ ForElse_aula40.py
@@ -0,0 +1,7 @@
+variavel = ['rarikmilkrai', 'monik', 'miguel']
+
+for valor in variavel:
+    if valor.startswith('r'):
+        print('começa com r: ',valor)
+    else:
+        print('Nao começa com r: ',valor)
