2e0c405951939b122a41d25c095338aacbee68f1
rarikmilkrai <rarikmilkrai05@gmail.com>
2023-11-04 14:57:55 -0300

    update

--- Listas_aula38.py
+++ Listas_aula38.py
@@ -57,3 +57,90 @@ while True:
         print(f'Que legal, VOCÊ GANHOU!!!! A palavra era: {secreto_temporario}')
     else:
         print(f'Apalavra secreta está assim: {secreto_temporario}')
+
+
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor)
+
+######################################################
+
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3
+lista = [10, 20, 30, 40]
+lista.append('Luiz')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(100, 5)
+print(lista[4])
+
+"""
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
+"""
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
+print(lista_a)
+print(lista_b)
+
+
+# usando for
+lista = ['rarik', 'monik', 'miguel']
+
+for nome in lista:
+    print(nome, type(nome))
+    
+    """
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
