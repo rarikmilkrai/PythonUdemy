00899bf8e1063b068cbd63168bdfcb99c8cd7e6e
rarikmilkrai <rarikmilkrai05@gmail.com>
2023-11-20 22:36:57 -0300

    voltando aos estudos

---
+++ operador_ternaria.py
@@ -0,0 +1,12 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicação> else <outro valor>
+"""
+# condição = 10 == 11
+# variavel = 'Valor' if condicao else 'Outro valor'
+#imprimir(variavel)
+# dígito = 9 # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 se dígito > 9 else dígito
+#imprimir(novo_digito)
+print ( 'Valor'  if  False  else  'Outro valor'  if  False  else  'Fim' )
